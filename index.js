38d415807957c796b2f3d9453fd89f39f126f4a2
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-02-14 22:55:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ function createTimeOutEvent(obj,dateStamp){
   obj.timeOutEvents.push(timeObj);
   return obj;
 }
+<<<<<<< HEAD
 function hoursWorkedOnDate(obj,dateStamp){
   let arrayIn = obj.timeInEvents ,arrayOut = obj.timeOutEvents,resIn = 0 ,resOut = 0 
   for (let element of arrayIn){
@@ -41,6 +42,31 @@ function hoursWorkedOnDate(obj,dateStamp){
   return (resOut-resIn) /100
   }
   
+=======
+/*function hoursWorkedOnDate(obj,dateStamp){
+  let arrayIn = obj.timeInEvents 
+  let arrayOut = obj.timeOutEvents
+  let resIn = 0 
+  let resOut = 0 
+  for (let element in arrayIn){
+    if(element.date == dateStamp) {resIn= element.hour ; break;}
+     }
+  for (let element in arrayOut){
+    if(element.date == dateStamp) {resOut= element.hour ; break;}
+  }
+  return (resOut-resIn) /100
+  }*/
+  function hoursWorkedOnDate(arr,date){
+let timeIn=arr.timeInEvents, timeOut = arr.timeOutEvents,inHours=0,outHours=0
+  for(let a of timeIn){
+    if(a.date==date) {inHours=a.hour; break;}
+  }
+   for(let a of timeOut){
+    if(a.date==date) {outHours=a.hour; break;}
+  }
+  return (outHours-inHours)/100 ;
+}
+>>>>>>> 8246fd1fd9723dc387c61fc573b3f244e5f601bd
 function wagesEarnedOnDate(obj,dateStamp){
   let hoursWorked = hoursWorkedOnDate(obj,dateStamp)
   let value = hoursWorked * obj.payPerHour
@@ -50,14 +76,28 @@ function wagesEarnedOnDate(obj,dateStamp){
 function allWagesFor(obj){
   let val = []
   for ( let odate of obj.timeInEvents){
+<<<<<<< HEAD
     val.push(odate.date)
   }
   return val.reduce(function(acc,value){return acc+= wagesEarnedOnDate(obj,value)},0)
 }
 function findEmployeeByFirstName(srcArray,firstName){
   return srcArray.find( ({ firstName }) => firstName === firstName );
+=======
+    val.push(odate)
+  }
+  return val.reduce(function(acc,value){return acc+= wagesEarnedOnDate(obj,value)},0)
+  
+}
+function findEmployeeByFirstName(srcArray,firstName){
+  return srcArray(firstName,index)
+>>>>>>> 8246fd1fd9723dc387c61fc573b3f244e5f601bd
 }
 function calculatePayroll(array){
    return array.reduce(function(total,element){return total+=allWagesFor(element)},0)
   
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 8246fd1fd9723dc387c61fc573b3f244e5f601bd
